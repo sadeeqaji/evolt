1d95286cb2dfc533ed4d6c4e49410be15bb0d54c
Collins Ogbuzuru <collinsogbuzuru@gmail.com>
2025-10-29 00:41:11 +0100

    chore: fix build issue

--- apps/evolt-fe/src/app/(dapp)/pools/[poolId]/page.tsx
+++ apps/evolt-fe/src/app/(dapp)/pools/[poolId]/page.tsx
@@ -2,14 +2,8 @@ import React, { Suspense } from "react";
 import PoolDetailClient from "@evolt/components/features/dashboard/PoolDetailClient";
 import PoolDetailSkeleton from "@evolt/components/features/dashboard/PoolDetailSkeleton";
 
-interface PageProps {
-  params: {
-    poolId: string;
-  };
-}
-
-export default async function Page({ params }: PageProps) {
-  const { poolId } = await params;
+export default async function Page(props: PageProps<"/pools/[poolId]">) {
+  const { poolId } = await props.params;
 
   return (
     <Suspense fallback={<PoolDetailSkeleton />}>
